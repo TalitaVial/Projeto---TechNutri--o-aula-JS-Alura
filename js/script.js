3c97a610ea5e38f71aa9257a9d3a0146fbd219b3
Talita Vial <talitavialsilva1@gmail.com>
2021-09-08 15:27:01 -0400

    condicionais

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 var paciente = document.querySelector("#primeiro-paciente");
 console.log(paciente);
 
@@ -9,10 +10,33 @@ var tdAltura = paciente.querySelector(".info-altura");
 
 var altura = tdAltura.textContent;
 
-var imc = peso / (altura * altura);
+var tdImc = paciente.querySelector('.info-imc');
+tdImc.textContent = imc;
+
+var pesoValido = true;
+var alturaValida = true;
+
+
+if(peso < 0 || peso >= 1000){
+  console.log("ivalid");
+  pesoValido = false;
+  tdImc.textContent = "Peso Inválido";
+}
+
+if(altura < 0 || altura >= 3.00){
+  console.log("invalid");
+  alturaValida = false;
+  tdImc.textContent = "Altura Inválida";
+}
+
+
+if(pesoValido && alturaValida){
+  var imc = peso / (altura * altura);
+  tdImc.textContent = imc;
+}
 
 console.log(tdAltura);
 console.log(tdPeso);
 console.log(imc);
-
+console.log(tdImc)
 
