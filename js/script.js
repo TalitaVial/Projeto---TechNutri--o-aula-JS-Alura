b2eab4934afe1f58affa423d8def8bb01e0a4703
Talita Vial <talitavialsilva1@gmail.com>
2021-09-19 08:56:42 -0400

    adicionando eventos JS

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 var pacientes = document.querySelectorAll('.paciente');
 console.log(pacientes);
 
+var titulo = document.querySelector(".titulo");
+
 for (var i = 0; i < pacientes.length; i++) {
 
   console.log(pacientes[i]);
@@ -25,12 +27,14 @@ for (var i = 0; i < pacientes.length; i++) {
     console.log('ivalid');
     pesoValido = false;
     tdImc.textContent = 'Peso Inválido';
+    paciente.classList.add("paciente-invalido");
   }
 
   if (altura < 0 || altura >= 3) {
     console.log('invalid');
     alturaValida = false;
     tdImc.textContent = 'Altura Inválida';
+    paciente.classList.add("paciente-invalido");
   }
 
   if (pesoValido && alturaValida) {
@@ -38,3 +42,43 @@ for (var i = 0; i < pacientes.length; i++) {
     tdImc.textContent = imc.toFixed(2);
   }
 }
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+
+botaoAdicionar.addEventListener("click", function(event){
+  event.preventDefault();
+  var form = document.querySelector("#form-adiciona");
+  var nome = form.nome.value;
+  var peso = form.peso.value;
+  var altura = form.altura.value;
+  var gordura = form.gordura.value;
+
+  var pacienteTr = document.createElement("tr");
+
+  var nomeTd = document.createElement("td");
+  var pesoTd = document.createElement("td");
+  var alturaTd = document.createElement("td");
+  var gorduraTd = document.createElement("td");
+  var imcTd = document.createElement("td");
+
+  nomeTd.textContent = nome;
+  pesoTd.textContent = peso;
+  alturaTd.textContent = altura;
+  gorduraTd.textContent = gordura;
+
+
+  pacienteTr.appendChild(nomeTd);
+  pacienteTr.appendChild(pesoTd);
+  pacienteTr.appendChild(alturaTd);
+  pacienteTr.appendChild(gorduraTd);
+
+
+  var tabela = document.querySelector("#tabela-pacientes");
+  tabela.appendChild(pacienteTr);
+
+  console.log(pacienteTr);
+ 
+
+
+
+});
