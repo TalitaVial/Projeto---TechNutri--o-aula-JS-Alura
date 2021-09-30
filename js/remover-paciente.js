d34e644549d8761eed0ce75caeb70e68609213e2
Talita Vial <talitavialsilva1@gmail.com>
2021-09-29 22:08:07 -0400

    Aula JS da Alura

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,6 +1,12 @@
 var tabela = document.querySelector("table")
 
 tabela.addEventListener("dblclick", function(event){
-  event.target.parentNode.remove()
+    if(event.target.tagName == "TD"){
+      event.target.parentNode.classList.add("fadeOut")
+      setTimeout(function(){
+        event.target.parentNode.remove()
+      }, 500)
+      
+    }
 
 })
