d36c399a1d008466df7a3b7e5fbddc878b751034
Talita Vial <talitavialsilva1@gmail.com>
2021-10-01 23:03:45 -0400

    Alura curso de introdução JS

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -6,13 +6,23 @@ botoaAdicionar.addEventListener('click', function(){
   xhr.open('GET', "https://api-pacientes.herokuapp.com/pacientes")
 
   xhr.addEventListener("load", function(){
-    var resposta = xhr.responseText
-    var pacientes = JSON.parse(resposta);
+    var erro = document.querySelector("#erro-ajax")
 
-    pacientes.forEach(function(paciente){
-      adicionaPacienteNaTabela(paciente);
-    })
- 
+    if(xhr.status == 200){
+      erro.classList.add("invisivel")
+      var resposta = xhr.responseText
+      var pacientes = JSON.parse(resposta);
+  
+      pacientes.forEach(function(paciente){
+        adicionaPacienteNaTabela(paciente);
+      })
+   
+    }else{
+      console.log(xhr.status)
+      console.log(xhr.responseText)
+      erro.classList.remove("invisivel")
+    }
+   
 
   })
 
